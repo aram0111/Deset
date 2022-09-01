0fc5bce29bb61a19f6e85ff7125c95ea6c686170
aram0111 <aramingk95@gmail.com>
2022-09-01 16:53:48 +0900

    commit

---
+++ src/main/java/com/example/Fescon/FesconApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fescon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FesconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FesconApplication.class, args);
+	}
+
+}
